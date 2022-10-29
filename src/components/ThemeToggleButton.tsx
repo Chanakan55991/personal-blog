403f7f15b90c7cf41955c5e741a7da4552531177
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-10-29 14:36:21 +0700

    feat(component): masthead

---
+++ src/components/ThemeToggleButton.tsx
@@ -0,0 +1,52 @@
+import { useState, useEffect } from 'react'
+import { IoSunny, IoMoon } from 'react-icons/io5/index'
+
+const themes = ['light', 'dark']
+
+export default function ThemeToggle() {
+  const [isMounted, setIsMounted] = useState(false)
+  const [theme, setTheme] = useState(() => {
+    if (import.meta.env.SSR) {
+      return undefined
+    }
+
+    if (typeof localStorage !== undefined && localStorage.getItem('theme') && localStorage.getItem('theme') === ('dark' || 'white'))
+      return localStorage.getItem('theme')
+
+    if (window.matchMedia('(prefers-color-scheme: dark)').matches)
+      return 'dark'
+
+    return 'light'
+  })
+  const toggleTheme = () => {
+    const t = theme === 'light' ? 'dark' : 'light' // light by default
+    localStorage.setItem('theme', t)
+    setTheme(t)
+  }
+
+  useEffect(() => {
+    const root = document.documentElement
+    if (theme === 'light') {
+      root.classList.remove('dark')
+    } else {
+      root.classList.add('dark')
+    }
+  }, [theme])
+
+  useEffect(() => {
+    setIsMounted(true)
+  }, [])
+
+  return isMounted ? (
+    <div className='inline-flex items-center p-[1px] rounded-3xl bg-orange-300 dark:bg-purple-600'>
+      {themes.map(t => {
+        const checked = t === theme
+        return (
+          <button key={t} className={`${checked ? 'bg-white text-black' : ''} cursor-pointer rounded-3xl p-2`} onClick={toggleTheme}>
+            {t === 'light' ? <IoSunny /> : <IoMoon />}
+          </button>
+        )
+      })}
+    </div>
+  ) : (<div />)
+}
