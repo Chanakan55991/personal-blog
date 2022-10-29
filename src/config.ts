ade10bc5a3ea84b2afc3db0f99388dd731a202ce
Chanakan Mungtin <chanakan5591@chanakancloud.net>
2022-10-29 09:18:48 +0700

    refactor(meta): update web info

--- src/config.ts
+++ src/config.ts
@@ -1,5 +1,6 @@
 // Place any global data in this file.
 // You can import this data from anywhere in your site by using the `import` keyword.
 
-export const SITE_TITLE = 'My personal website.';
-export const SITE_DESCRIPTION = 'Welcome to my website!';
+export const SITE_TITLE = 'Blog - Chanakan Mungtin';
+export const SITE_DESCRIPTION = 'A random blog posts I made from time to time';
+export const HOMEPAGE_URL = 'https://www.chanakancloud.net'
